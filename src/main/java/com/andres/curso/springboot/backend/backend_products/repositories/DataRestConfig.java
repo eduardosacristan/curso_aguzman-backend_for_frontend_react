4a2fc19ffd090ee7d0a7391a10ff5c9e1d580c6d
eduardosacristan <eduardosacristan@outlook.es>
2025-10-13 15:26:56 +0100

    added CORS

---
+++ src/main/java/com/andres/curso/springboot/backend/backend_products/repositories/DataRestConfig.java
@@ -0,0 +1,24 @@
+package com.andres.curso.springboot.backend.backend_products.repositories;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.andres.curso.springboot.backend.backend_products.entities.Product;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer{
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Product.class);
+        // De esta manera hacemos que se muestre el id de cada producto
+    }
+
+    
+
+    
+
+
+}
--- src/main/java/com/andres/curso/springboot/backend/backend_products/repositories/ProductRepository.java
+++ src/main/java/com/andres/curso/springboot/backend/backend_products/repositories/ProductRepository.java
@@ -2,9 +2,11 @@ package com.andres.curso.springboot.backend.backend_products.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.andres.curso.springboot.backend.backend_products.entities.Product;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestResource(path = "products") //Esta anotacion genera totalmente el CRUD con su controller 
 public interface ProductRepository extends CrudRepository<Product, Long> {
 }
